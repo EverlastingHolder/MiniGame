42b5291c5378d8ca6f51800700e080bb4b7084d7
Roma <moshkovcevroma@gmail.com>
2022-11-16 12:08:11 +0700

    Initial Commit

---
+++ MiniGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniGame
+//
+//  Created by Роман Мошковцев on 16.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
